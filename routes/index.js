42edcbbff6ee938cbb7df92b15652892ef83e12b
gusyang <gus.yang@aol.com>
2014-12-03 17:52:37 -0800

    add ebook

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+  console.log(__dirname);
 });
 
 module.exports = router;
