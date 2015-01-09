c20c9a45895d572e7fb57e83b3dc8c8f79ab59cb
Gus Yang <gus.yang@aol.com>
2015-01-08 22:41:38 -0800

    modify chat

--- chat.js
+++ chat.js
@@ -4,28 +4,47 @@ var chatServer = net.createServer(), clientList = [];
 chatServer.on("connection",function(client){
 	client.name = client.remoteAddress + " : " + client.remotePort;
 
-	client.write("Hi"+client.name+"!\n");	
+	client.write("Welcome "+client.name+"!\n");	
+    console.log(client.name + " joined");
+
+    //add all client to a list
 	clientList.push(client);
 
-	//client.write("Bye!\n");
 	client.on("data",function(data){
-		//broadcast(data,client);
-		for(var i=0; i<clientList.length; i+=1){
-			if(client != clientList[i])
-			clientList[i].write(client.name + " Say " + data);
-		}
+		broadcast(data,client);
 	});
+
+    client.on("end",function(){
+        console.log(client.name + "quit");
+        //remove client from client list after close.
+        clientList.splice(clientList.indexOf(client),1);
+    });
+
+    client.on("error",function(e){
+        //raise exception
+        console.log(e);
+    });
 });
 
 function broadcast(message,client){
-	for(var i=0;i<=clientList.length;i+=1){
-		if(client !== clientList[i])
-			//console.log(clientList[i]);
-			clientList[i].write(client.name + " Says " + message);
+    var cleanup = [];
+
+	clientList.forEach(function(c){
+        console.log(client.name + "broadcast");
+        //do not broadcast to client self
+		if(client !== c){
+            if(c.writable){
+			    c.write(client.name + " Says " + message);
+            }else{
+                cleanup.push(c);
+                c.destroy();
+            }
 		}
-		console.log(data);
-	
-}
+    cleanup.forEach(function(clean){
+        clientList.splice(clientList.indexOf(clean),1);
+    });
+    });
+};
 
 chatServer.listen(9000);
 console.log("listening on port:9000");
